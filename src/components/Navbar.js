cbe2a21165018cd3c39853b735ec79cc600e8004
nishadbhujbal <nishadbhujbal128@gmail.com>
2024-06-13 13:45:06 +0530

    Added couple more buttons to improve functionality and tweaked the responsive design

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,57 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Navbar(props) {
+  const [myStyle, setMyStyle] = useState({
+    color: "black",
+    backgroundColor: "white",
+  });
+
+  const [btnText, setBtnText] = useState("Enable Dark Mode");
+
+  const toggleStyle = () => {
+    if (myStyle.color === "black") {
+      setMyStyle({
+        color: "white",
+        backgroundColor: "black",
+      });
+      setBtnText("Enable Dark Mode");
+    } else {
+      setMyStyle({
+        color: "black",
+        backgroundColor: "white",
+      });
+      setBtnText("Enable Light Mode");
+    }
+  };
+
   return (
     <>
-      <nav className="flex justify-between items-center w-full bg-blue-600 p-2 px-6 text-white  ">
-        <div id="name" className="font-semibold text-2xl">
-          {props.title}
+      <nav
+        className="fixed top-0 right-0 left-0 flex flex-col-reverse md:flex-row justify-between items-center border-b-2 w-full bg-blue-600 p-2 px-6 text-white gap-2"
+        style={myStyle}
+      >
+        <div>
+          <button
+            onClick={toggleStyle}
+            className="rounded-xl font-semibold p-2 border border-blue-500 bg-white text-blue-500 hover:bg-blue-500 hover:text-white"
+          >
+            {btnText}
+          </button>
         </div>
-        <ul id="menuItems" className="flex gap-4 text-lg font-semibold">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/About">About</Link>
-          </li>
-        </ul>
+        <div className="flex flex-row justify-around md:justify-between items-center w-full md:w-1/2">
+          <div id="name" className="font-semibold text-2xl ">
+            {props.title}
+          </div>
+          <ul id="menuItems" className="flex gap-4 text-lg font-semibold">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/About">About</Link>
+            </li>
+          </ul>
+        </div>
       </nav>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,19 +2,37 @@ import { useState } from "react";
 
 function TextForm(props) {
   const [text, setText] = useState("");
-  const handleUpClick = () => {
+
+  const convertUpperCase = () => {
     let newText = text.toUpperCase();
     setText(newText);
   };
 
-  const handleLowClick = () => {
+  const convertLowerCase = () => {
     let newText = text.toLowerCase();
     setText(newText);
   };
 
-  const handleWhiteSpaceClick = () => {
+  const handleWhiteSpace = () => {
     let newText = text.replace(/\s/g, "");
     setText(newText);
+  };
+
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
+
+  const handleItalic = () => {
+    let newText = text;
+    document.getElementById("para").innerHTML = newText.italics();
+    setText(newText);
+  };
+
+  const handleCopy = () => {
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
   };
 
   const handleClearClick = () => {
@@ -29,23 +47,23 @@ function TextForm(props) {
 
   return (
     <>
-      <div className="rounded-xl h-full flex flex-col items-center justify-center gap-8">
-        <div className="flex flex-col mt-8 m-4 p-2 md:p-4 border border-black rounded-2xl md:flex-row md:w-[90vh] md:h-[50vh] ">
-          <div className=" w-full md:w-1/2 p-4 md:border-r-2">
-            <h1 className="text-xl md:text-3xl my-2 md:my-4 font-semibold">
+      <div className="mt-20 flex flex-col items-center justify-center gap-8">
+        <div className="flex flex-col mt-8 m-4 p-2 lg:p-4 border border-black rounded-2xl lg:flex-row lg:w-[90vh] lg:h-[50vh] ">
+          <div className=" w-full lg:w-1/2 p-4 lg:border-r-2">
+            <h1 className="text-xl lg:text-3xl my-2 lg:my-4 font-semibold">
               {props.heading1}
             </h1>
             <textarea
               name="text"
               id="myBox"
-              className="border border-black rounded-2xl w-full h-[170px] md:h-1/2 pt-2 px-2 scrollbar-hide"
+              className="border border-black rounded-2xl w-full h-[170px] lg:h-1/2 pt-2 px-2 scrollbar-hide"
               value={text}
               onChange={handleOnChange}
             ></textarea>
           </div>
 
-          <div className="w-full md:w-1/2 p-4">
-            <h1 className="text-xl md:text-3xl my-2 md:my-4 font-semibold">
+          <div className="w-full lg:w-1/2 p-4">
+            <h1 className="text-xl lg:text-3xl my-2 lg:my-4 font-semibold">
               {props.heading2}
             </h1>
             <p className="text-lg">{text.length} characters</p>
@@ -58,24 +76,42 @@ function TextForm(props) {
           </div>
         </div>
 
-        <div className="flex flex-col md:flex-row justify-center gap-4">
+        <div className="flex flex-row flex-wrap justify-center gap-4 md:px-8">
           <button
             className="rounded-xl font-semibold px-6 py-2 border border-blue-500 bg-white text-blue-500 hover:bg-blue-500 hover:text-white"
-            onClick={handleUpClick}
+            onClick={convertUpperCase}
           >
             Convert to Uppercase
           </button>
           <button
             className="rounded-xl font-semibold px-6 py-2 border border-blue-500 bg-white text-blue-500 hover:bg-blue-500 hover:text-white"
-            onClick={handleLowClick}
+            onClick={convertLowerCase}
           >
             Convert to Lowercase
           </button>
           <button
             className="rounded-xl font-semibold px-6 py-2 border border-blue-500 bg-white text-blue-500 hover:bg-blue-500 hover:text-white"
-            onClick={handleWhiteSpaceClick}
+            onClick={handleWhiteSpace}
           >
             Remove White Spaces
+          </button>
+          <button
+            className="rounded-xl font-semibold px-6 py-2 border border-blue-500 bg-white text-blue-500 hover:bg-blue-500 hover:text-white"
+            onClick={handleExtraSpaces}
+          >
+            Remove Extra Spaces
+          </button>
+          <button
+            className="rounded-xl font-semibold px-6 py-2 border border-blue-500 bg-white text-blue-500 hover:bg-blue-500 hover:text-white"
+            onClick={handleItalic}
+          >
+            Convert to Italic
+          </button>
+          <button
+            className="rounded-xl font-semibold px-6 py-2 border border-blue-500 bg-white text-blue-500 hover:bg-blue-500 hover:text-white"
+            onClick={handleCopy}
+          >
+            Copy Text
           </button>
           <button
             className="rounded-xl font-semibold px-6 py-2 border border-blue-500 bg-white text-blue-500 hover:bg-blue-500 hover:text-white"
@@ -87,7 +123,9 @@ function TextForm(props) {
 
         <div id="preview" className="flex flex-col px-8 mb-8  w-full gap-4">
           <h2 className="text-2xl font-semibold">Preview</h2>
-          <p className="text-lg whitespace-pre-wrap break-words">{text}</p>
+          <p id="para" className="text-lg whitespace-pre-wrap break-words">
+            {text}
+          </p>
         </div>
       </div>
     </>
